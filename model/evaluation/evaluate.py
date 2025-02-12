88ac3202f637dd70ea85f742a7c1303dcac673e4
Deeptanshuu <laldeeptanshu96@gmail.com>
2025-02-12 18:09:52 +0530

    rea

--- model/evaluation/evaluate.py
+++ model/evaluation/evaluate.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import (
     roc_auc_score, precision_recall_fscore_support, 
-    confusion_matrix, roc_curve, hamming_loss, 
+    confusion_matrix, hamming_loss, 
     accuracy_score, precision_score, recall_score, f1_score,
     brier_score_loss
 )
